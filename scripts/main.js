49b24d0a807b4aa0d54ed931234f575163d6df5e
Dean Coakley <dean.s.coakley@gmail.com>
2017-01-20 03:06:55 +0000

    Fixed font url. Added api request. (WIP)

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,24 @@ var helloCork = function() {
   console.log('Hello Cork');
 };
 
+var getUpcomingEventsAsync = function() {
+	//Calling another meetup just for test because CorkJS has no upcoming meetups
+	var url = "http://api.meetup.com/openstack-cork/events?&sign=true&photo-host=public&page=20&key=/*YOUR_API_KEY*/";
+	var request = new XMLHttpRequest();
+	request.open( "GET", url, true);
+	request.send(null);	
+}
+
 helloCork();
+var data = getUpcomingEventsAsync();
+
+/*$.ajax({
+	type: "GET",
+	dataType: "jsonp",
+	url: "url"
+})*/
+
+var test = function() {
+	document.getElementsByClassName("meetup-title").innerHTML = data[0].name();
+	console.log("Works");
+}
