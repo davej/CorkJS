5ad032be203cc425dba62ae2d37fcdf0330f2242
Dean Coakley <dean.s.coakley@gmail.com>
2017-01-22 03:59:50 +0000

    Added favicon. Added signed urls. Added meetup status rewriting. Added descriptions. Added past event request. General code improvements

--- scripts/main.js
+++ scripts/main.js
@@ -2,36 +2,49 @@ var helloCork = function() {
   	console.log('Hello Cork');
 };
 
+var signedUrlUpcoming = "https://api.meetup.com/cork-javascript-meetup/events?photo-host=public&page=20&sig_id=210143296&sig=d07ac4460173bc86aa05e0c5b65acd79b2b8ae1b";
+var signedUrlPast = "https://api.meetup.com/cork-javascript-meetup/events?photo-host=public&page=20&sig_id=210143296&status=past&sig=052868f6f4d1f5a525fa6ca28a74e771165ad4ea";
+
 var getUpcomingEvents = function() {
-	//Calling another meetup just for test because CorkJS has no upcoming meetups
-	var signedTestUrl = "https://api.meetup.com/openstack-cork/events?photo-host=public&page=20&sig_id=210143296&omit=group&_=1485037467231&sig=3a5e1b900646da97688cc54f55f67827eafd814d";
-	var signedUrl = "https://api.meetup.com/openstack-cork/events?photo-host=public&page=20&sig_id=210143296&sig=d07ac4460173bc86aa05e0c5b65acd79b2b8ae1b";
-	
 	var call = $.ajax({
 		type: "GET",
 		dataType: "jsonp",
-		url: signedUrl
+		url: signedUrlUpcoming
 	});
+	//console.log(call);
+	updateDOM(call, true);
+	return call;
+}
 
-	console.log(call);
+var getPastEvents = function() {
+	var call = $.ajax({
+		type: "GET",
+		dataType: "jsonp",
+		url: signedUrlPast 
+	});
+	//console.log(call);
+	updateDOM(call, false);
+}
 
+var updateDOM = function(call, upcoming) {
 	//When ajax call is complete
 	$(document).ajaxStop(function() {
 		var events = (call.responseJSON.data);
-		console.log(events);
-
-		updateDOM(events);
+		//console.log(events);
+		if (events.length != 0) {
+			var index = 0;
+			if (!upcoming) {
+				index = events.length - 1;
+				$("#latest-status").text("Previous");
+			} else {
+				$("#latest-status").text("Upcoming");
+			}
+			$("#latest-title").text(events[index].name);
+			$("#latest-desc").replaceWith(events[index].description);
+		}
 	});
 }
 
-var updateDOM = function(events) {
-	if (events.length > 0) {
-		$("#latest-title").text( events[0].name);
-		$("#latest-desc").text(events[0].description);
-	} else {
-		$("#latest-title").text("No upcoming events");
-	}
-}
-
 helloCork();
+getPastEvents();
 getUpcomingEvents();
