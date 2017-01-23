16a1a944c7d32cefe65d96780534eefd49b32216
Dean Coakley <dean.s.coakley@gmail.com>
2017-01-23 00:57:03 +0000

    Added date/time of meetup

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,11 @@ var helloCork = function() {
 };
 
 var signedUrl = "https://api.meetup.com/cork-javascript-meetup/events?desc=true&photo-host=public&page=20&sig_id=210143296&status=upcoming%2Cpast&sig=0607ea8f24a049a251ec629255d393a89c8ece7f";
-var call;
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+var days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
 
 var getMeetups = function() {
-	call = $.ajax({
+	var call = $.ajax({
 		type: "GET",
 		dataType: "jsonp",
 		url: signedUrl,
@@ -22,16 +23,25 @@ var updateDOM = function(responseJSON) {
 
 	//Checks if no meetups were returned
 	if (events.length != 0) {
+
+		//Replace banner text
 		if (events[0].status == "past") {
 			document.getElementById("latest-status").innerHTML = "Previous";
 		} else {
 			document.getElementById("latest-status").innerHTML = "Upcoming";
 		}
 
+		//Replace title, description
 		document.getElementById("latest-title").innerHTML = events[0].name;
 		document.getElementById("latest-desc").innerHTML = events[0].description;
+
+		//Replace date
+		var date = new Date(events[0].time);
+		document.getElementById("latest-date").innerHTML = (days[date.getDay() -1] + " " + date.getDate() + " " + months[date.getMonth()] + ", " + date.getFullYear() + " - " 
+			+ date.getHours() + ":" + date.getMinutes() );
+
 	} else {
-		//There's no reason for this to ever happen. Unless group is deleted, should is there any point of this check?
+		//There's no reason for this to ever happen. Unless group is deleted, is there any point of this check?
 		console.log("ERROR: No meetups returned from request.");   
 	}
 }
