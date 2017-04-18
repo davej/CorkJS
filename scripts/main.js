c3522d4170a5045904e61d463db017445401897a
Dean Coakley <dean.s.coakley@gmail.com>
2017-04-18 21:10:19 +0100

    Remove redundant function. Remove redundant css rule. Add attendance counter. Add embed map

--- scripts/main.js
+++ scripts/main.js
@@ -7,17 +7,13 @@ var months = [
 ];
 var days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
 
-var jsonp = function(url) {
+var getMeetups = function(url) {
   var head = document.head;
   var script = document.createElement('script');
 
   script.setAttribute('src', url);
   head.appendChild(script);
   head.removeChild(script);
-}
-
-var getMeetups = function() {
-  jsonp(signedUrl);
 }
 
 var updateDOM = function(responseJSON) {
@@ -62,6 +58,22 @@ var updateDOM = function(responseJSON) {
       ' - [ <a href="http://maps.google.com/?q=' + location.name +
       '" target="_blank">Directions</a> ]'
     );
+
+  // Add embed map
+  var map = document.getElementById('map');
+
+  map.src = 'https://www.google.com/maps/embed/v1/place?' +
+    'key=AIzaSyC9JxGGhS0Xr1429ae62E3MEfk227kqDIA&q=' +
+    location.address_1;
+
+  // Add attendance count
+  if (latestEvent.status == 'past') {
+    document.getElementById("rsvp-count").innerHTML = 'Attended: ' +
+      latestEvent.yes_rsvp_count;
+  } else {
+    document.getElementById("rsvp-count").innerHTML = 'Attending: ' +
+      latestEvent.yes_rsvp_count;
+  }
 }
 
-getMeetups();
+getMeetups(signedUrl);
