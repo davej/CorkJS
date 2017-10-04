c90d507111d12e02607b34f455c4dd7319bb1970
Dean <dean.s.coakley@gmail.com>
2017-10-04 12:03:01 +0100

    ES6 refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,23 @@
-var signedUrl = 'https://api.meetup.com/cork-javascript-meetup/events?' +
+const signedUrl = 'https://api.meetup.com/cork-javascript-meetup/events?' +
   'desc=true&photo-host=public&page=20&sig_id=210143296&callback=updateDOM&' +
   'status=past%2Cupcoming&sig=4a36a805aac0c8de4199cb3008a95f9fa292ca9b';
-var months = [
+const months = [
   'Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept',
   'Oct', 'Nov', 'Dec'
 ];
-var days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
+const days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
 
-var getMeetups = function(url) {
-  var head = document.head;
-  var script = document.createElement('script');
+let getMeetups = function(url) {
+  let head = document.head;
+  let script = document.createElement('script');
 
   script.setAttribute('src', url);
   head.appendChild(script);
   head.removeChild(script);
 }
 
-var updateDOM = function(responseJSON) {
-  var events = responseJSON.data;
+let updateDOM = function(responseJSON) {
+  let events = responseJSON.data;
 
   // Checks if no meetups were returned
   if (!(events && events.length > 0)) {
@@ -25,7 +25,7 @@ var updateDOM = function(responseJSON) {
     return;
   };
 
-  var latestEvent = events[0];
+  let latestEvent = events[0];
 
   // Replace banner text
   if (latestEvent.status == 'past') {
@@ -39,29 +39,28 @@ var updateDOM = function(responseJSON) {
   document.getElementById('latest-desc').innerHTML = latestEvent.description;
   
   // Replace date text
-  var date = new Date(latestEvent.time);
-  document.getElementById('latest-date').innerHTML =
-    (
-      days[date.getDay() -1] + ' ' + date.getDate() + ' ' +
-      months[date.getMonth()] + ', ' + date.getFullYear() + ' - ' +
-      date.getHours() + ':' + date.getMinutes()
-    );
+  let date = new Date(latestEvent.time);
+  document.getElementById('latest-date').innerHTML = (`
+    ${days[date.getDay() -1]} ${date.getDate()}
+    ${months[date.getMonth()]}, ${date.getFullYear()} - 
+    ${date.getHours()}:${date.getMinutes()}    
+  `);
 
   // Add link to title
-  var link = latestEvent.link;
+  let link = latestEvent.link;
   document.getElementById('latest-link').href = link;
 
   // Replace location data
-  var location = latestEvent.venue;
+  let location = latestEvent.venue;
   document.getElementById('latest-location').innerHTML =
-    (
-      location.name + '<br>' + location.address_1 +
-      ' - [ <a href="http://maps.google.com/?q=' + location.name +
-      '" target="_blank">Directions</a> ]'
-    );
+    (`
+      ${location.name}<br>${location.address_1}
+       - [ <a href="http://maps.google.com/?q=${location.name}
+        target="_blank">Directions</a> ]    
+    `);
 
   // Add embed map
-  var map = document.getElementById('map');
+  let map = document.getElementById('map');
   map.src = 'https://www.google.com/maps/embed/v1/place?' +
     'key=AIzaSyC9JxGGhS0Xr1429ae62E3MEfk227kqDIA&q=' +
     location.name + ', Cork, Ireland' + '&zoom=17';
